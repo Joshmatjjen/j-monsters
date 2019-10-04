5e5feb5a8be5a8653de90dd4fbbba7481bcecf6c
Joshmatjjen <joshmatparrot@gmail.com>
2019-10-04 17:12:53 +0100

    Updates

---
+++ precache-manifest.bf9e8418224c80dfd833e47933fe7d68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4797715227b8579761e820bd2f36e3a",
+    "url": "/j-monsters/index.html"
+  },
+  {
+    "revision": "bbf0a069bf0d9c28aaf7",
+    "url": "/j-monsters/static/css/main.b1185973.chunk.css"
+  },
+  {
+    "revision": "c43acb01d1610c508c85",
+    "url": "/j-monsters/static/js/2.00e2b367.chunk.js"
+  },
+  {
+    "revision": "bbf0a069bf0d9c28aaf7",
+    "url": "/j-monsters/static/js/main.fee0156b.chunk.js"
+  },
+  {
+    "revision": "e23e01aa7b5bbf7eda3b",
+    "url": "/j-monsters/static/js/runtime-main.1563c565.js"
+  }
+]);
